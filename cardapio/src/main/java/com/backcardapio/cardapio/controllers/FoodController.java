201ff7697bf1bb1b5bef3617705cacf1972f5de1
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2025-06-21 21:18:54 -0300

    criados bd e entity e inteface repository food

--- cardapio/src/main/java/com/backcardapio/cardapio/controllers/FoodController.java
+++ cardapio/src/main/java/com/backcardapio/cardapio/controllers/FoodController.java
@@ -2,17 +2,28 @@ package com.backcardapio.cardapio.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.backcardapio.cardapio.food.Food;
+import com.backcardapio.cardapio.food.FoodRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
 @RequestMapping("/foods")
 public class FoodController {
+    @Autowired
+    private FoodRepository repository;
 
-    @GetMapping("")
-    public void getAll() {
+    @GetMapping
+    public List<Food> getAll() {
         
+        List<Food> foodList = repository.findAll();
+        return foodList;
     }
     
 
